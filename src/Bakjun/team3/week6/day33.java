2747666efa759a7339f0a21179589e33836daf56
coding-faster <berichmore@naver.com>
2025-12-18 00:12:06 +0900

    251218 오전 00:12 코테

---
+++ src/Bakjun/team3/week6/day33.java
@@ -0,0 +1,29 @@
+package Bakjun.team3.week6;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+//https://www.acmicpc.net/problem/1037
+public class day33 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = sc.nextInt();
+
+        }
+
+        Arrays.sort(arr);
+
+        int result = arr[0] * arr[count-1];
+        System.out.println(result);
+    }
+}
+
+
+// 1. 약수문제는 정렬해서 양 끝을 곱한다.
+// 2. Arrays.sort는 오름차순 정렬
+// 3. 문제 속에 힌트가 반드시 있다.
