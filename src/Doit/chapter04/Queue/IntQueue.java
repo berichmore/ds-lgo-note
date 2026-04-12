12bc461668746140735481c0d1d7c03e2e273836
coder_zero <berichmore@naver.com>
2026-04-13 00:33:42 +0900

    doit 자료구조 알고리즘 - queue p.154까지 - queue 구현 연습 -2

--- src/Doit/chapter04/Queue/IntQueue.java
+++ src/Doit/chapter04/Queue/IntQueue.java
@@ -68,4 +68,59 @@ public class IntQueue {
         }
         return x;
     }
+
+    // 큐에서 데이터를 peek(프론트 데이터를 들여다 봄)
+    public int peek() throws EmptyIntQueueException {
+        if (num <= 0) {
+            throw new EmptyIntQueueException(); // 큐가 비어 있음
+        }
+        return queue[front];
+    }
+
+    // 큐를 비움
+    public void clear() {
+        num = front = rear = 0;
+    }
+
+    // 큐에서 x를 검색하여 인덱스(찾지 못하면 -1)를 반환
+    public int indexOf(int x) {
+        for (int i = 0; i < num; i++) {
+            int index = (i + front) % capacity;
+            if (queue[index] == x) {
+            }
+            return index;
+        }
+        return -1;
+    }
+    // 큐의 용량을 반환
+    public int getCapacity() {
+        return capacity;
+    }
+
+    // 큐에 쌓여 있는 데이터 개수를 반환
+    public int size() {
+        return num;
+    }
+
+    //  큐가 비어 있나요?
+    public boolean isEmpty() {
+        return num <= 0;
+    }
+
+    // 큐가 가득 찼나요?
+    public boolean isFull() {
+        return num >= capacity;
+    }
+
+    // 큐 안의 모든 데이터를 front -> rear 순서로 출력
+    public void dump() {
+        if (num <= 0) {
+            System.out.println("큐가 비어 있습니다.");
+        } else {
+            for (int i = 0; i < num; i++) {
+                 System.out.println(queue[(i + front) % capacity] + " ");
+                System.out.println();
+            }
+        }
+    }
 }
