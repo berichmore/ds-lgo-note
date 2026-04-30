7b6e747daf54b9113b145417419d518d9548c1fb
coding-faster <berichmore@naver.com>
2026-04-30 23:38:04 +0900

    JOIN연습
    - INNER JOIN 문제 3

--- src/조인연습/JoinStudy.java
+++ src/조인연습/JoinStudy.java
@@ -28,5 +28,17 @@ public class JoinStudy {
 
 
 
+
+    문제3)
+    모든 고객의 이름(first_name, last_name)과
+    그 고객이 영화를 빌린 날짜(rental_date)를 함께 보고 싶습니다.
+
+
+    SELECT c.first_name, c.last_name, r.rental_date
+    FROM customer c
+    INNER JOIN rental r ON c.customer_id = r.customer_id;
+
+
+
      */
 }
